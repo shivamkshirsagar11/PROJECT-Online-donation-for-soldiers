3b59a8168d613a40a9c8dccf79191c6c1a1bf2fe
shivamkshirsagar11 <84304393+shivamkshirsagar11@users.noreply.github.com>
2021-07-04 15:58:52 +0530

    Delete ICTWIIb_Project_CE053_CE094_CE070 directory

--- ICTWIIb_Project_CE053_CE094_CE070/connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-$link = mysqli_connect('localhost','root','','php_p') or die("Could not connect to the database!!!");
-?>
--- ICTWIIb_Project_CE053_CE094_CE070/time_code.php
+++
@@ -1,4 +0,0 @@
-<?php
- date_default_timezone_set('Asia/Kolkata');
-$date = (date("F d, Y h:i:s A", time()));
-?>
